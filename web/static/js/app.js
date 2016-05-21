3c49f8b2f43fa2534c47697a8f010c4a61cce091
Benjamin Eckel <bhelx@simst.im>
2016-05-21 11:50:14 -0500

    https tile

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -161,7 +161,7 @@ ready(() => {
 
   map.setView([29.951066, -90.071532], 13);
 
-  L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
+  L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
       attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
       subdomains: 'abcd',
       minZoom: 12,
